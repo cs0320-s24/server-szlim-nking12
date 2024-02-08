5e7e065d353a1571bbc02599c396d41a044f8c82
Sophia Lim <sophia_lim@brown.edugit config --global user.name Sophia Lim>
2024-02-08 14:39:06 -0500

    setup: added csv

---
+++ src/main/java/edu/brown/cs/student/main/exceptions/FactoryFailureException.java
@@ -0,0 +1,16 @@
+package edu.brown.cs.student.main.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is an error provided to catch any error that may occur when you create an object from a row.
+ */
+public class FactoryFailureException extends Exception {
+  final List<String> row;
+
+  public FactoryFailureException(String message, List<String> row) {
+    super(message);
+    this.row = new ArrayList<>(row);
+  }
+}
---
+++ src/main/java/edu/brown/cs/student/main/objects/Person.java
@@ -0,0 +1,24 @@
+package edu.brown.cs.student.main.objects;
+
+/**
+ * The Person class represents an object to store information about an individual, including first
+ * name, last name, and nickname.
+ */
+public class Person {
+  public String firstName;
+  public String lastName;
+  public String nickname;
+
+  /**
+   * Constructs a Person with the specified first name, last name, and nickname.
+   *
+   * @param first The first name of the person.
+   * @param last The last name of the person.
+   * @param nickname The nickname of the person.
+   */
+  public Person(String first, String last, String nickname) {
+    this.firstName = first;
+    this.lastName = last;
+    this.nickname = nickname;
+  }
+}
