b4e2f438f1d40000379a94cce765a5763d56444c
Sophia Lim <sophia_lim@brown.edugit config --global user.name Sophia Lim>
2024-02-15 09:37:42 -0500

    feat: json adapter

--- src/main/java/edu/brown/cs/student/main/server/CensusResponse.java
+++ src/main/java/edu/brown/cs/student/main/server/CensusResponse.java
@@ -1,3 +1,8 @@
 package edu.brown.cs.student.main.server;
 
-public record CensusResponse(String NAME, String S2802_C03_022E) {}
+import com.squareup.moshi.Json;
+import com.squareup.moshi.JsonClass;
+
+@JsonClass(generateAdapter = true)
+public record CensusResponse(
+    @Json(name = "NAME") String name, @Json(name = "S2802_C03_022E") String S2802_C03_022E) {}
