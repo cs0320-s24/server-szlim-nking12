ee267269db6ad690449d23ebb1fd4f8d789ddc9c
Natalie <natalie_king@brown.edu>
2024-02-15 21:29:26 -0500

    feat: more work on testing file

--- src/test/java/edu/brown/cs/student/tests/TestBroadband.java
+++ src/test/java/edu/brown/cs/student/tests/TestBroadband.java
@@ -1,8 +1,17 @@
 package edu.brown.cs.student.tests;
 
 import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
+import edu.brown.cs.student.main.server.ACSDataSource;
+import edu.brown.cs.student.main.server.BroadbandHandler;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -26,4 +35,52 @@ public class TestBroadband {
   private final Type mapStringObject = Types.newParameterizedType(Map.class, String.class, String.class);
   private JsonAdapter<Map<String, String>> adapter;
   //private JsonAdapter<> weatherDataAdapter;
+
+  @BeforeEach
+  public void setup() {
+    // Re-initialize parser, state, etc. for every test method
+
+    // Use *MOCKED* data when in this test environment.
+    // Notice that the WeatherHandler code doesn't need to care whether it has
+    // "real" data or "fake" data. Good separation of concerns enables better testing.
+
+    ArrayList<List<String>> list = new ArrayList<List<String>>();
+    HashSet<String> coll = new HashSet<>();
+    coll.add("Los Angeles County, California");
+    coll.add("89.9");
+
+    List<String> strList = new ArrayList<>();
+    strList.addAll(coll);
+    list.add(strList);
+
+    ACSDataSource mockedSource = new MockedCensusSource(list);
+    Spark.get("broadband", new BroadbandHandler(mockedSource));
+    Spark.awaitInitialization(); // don't continue until the server is listening
+
+    // New Moshi adapter for responses (and requests, too; see a few lines below)
+    //   For more on this, see the Server gearup.
+    Moshi moshi = new Moshi.Builder().build();
+    adapter = moshi.adapter(mapStringObject);
+  }
+
+  @AfterEach
+  public void tearDown() {
+    // Gracefully stop Spark listening on both endpoints
+    Spark.unmap("broadband");
+    Spark.awaitStop(); // don't proceed until the server is stopped
+  }
+
+  private HttpURLConnection tryRequest(String apiCall) throws IOException {
+    // Configure the connection (but don't actually send a request yet)
+    URL requestURL = new URL("http://localhost:" + Spark.port() + "/" + apiCall);
+    HttpURLConnection clientConnection = (HttpURLConnection) requestURL.openConnection();
+    // The request body contains a Json object
+    clientConnection.setRequestProperty("Content-Type", "application/json");
+    // We're expecting a Json object in the response body
+    clientConnection.setRequestProperty("Accept", "application/json");
+    clientConnection.connect();
+    return clientConnection;
+  }
+
+
 }
